e480a967c54cc41ca6ebce5a2a3d1eee7d992c11
Kenechukwu Nwafor <38596033+Kenechukwu-val@users.noreply.github.com>
2019-07-23 12:27:07 +0100

    Made some changes

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.pageNotfound);
+app.use(errorController.get404);
 
-app.listen(3000, () => {
-    console.log('Server Running')
-});
+app.listen(3000);
--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
-const Product = require('../models/productModel');
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -10,15 +10,15 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-exports.postAddProduct =  (req, res, next) => {
+exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
-}
+};
 
-exports.getProduct = (req, res, next) => {
+exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('shop/product-list', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
@@ -27,4 +27,4 @@ exports.getProduct = (req, res, next) => {
       productCSS: true
     });
   });
-}
+};
